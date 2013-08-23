bb9a67c703a7a39c56924921480df0f39c24bdad
kidon-fedora-local <kidon@sterilngtrader.com>
2013-08-22 22:04:07 -0500

    Change About Us page layout

--- wp-content/themes/wp-bootstrap/page-about-us.php
+++ wp-content/themes/wp-bootstrap/page-about-us.php
@@ -1,21 +1,79 @@
 <?php get_header() ?>
 <div class="content-container container-fluid">
 
+	<header class="pageHeader border-bottom">
+		<h1>ABOUT US</h1>
+	</header>
+
+	<p> The National Organization for Renal Disease is a nonprofit health agency dedicated to preventing kidney (renal) disease, diabetes and high blood pressure through education and research. </p>
+
 	<div class="row-fluid">
+		<div class="span4">
+			<img src="http://lorempixel.com/300/150/people/10" alt="img1" class="img-polaroid">
+			<header class="postHeader">
+				<h3>Our Mission</h3>
+			</header>
+			<p>Lerem ipsum can be a very good way to modify each string value in website</p>
+		</div>
+		<div class="span4">
+			<img src="http://lorempixel.com/300/150/people/7" alt="img2" class="img-polaroid">
+			<header class="postHeader">
+				<h3>Our Program</h3>
+			</header>
+			<p>Lerem ipsum can be a very good way to modify each string value in website</p>
+		</div>
+		<div class="span4">
+			<img src="http://lorempixel.com/300/150/people/8" alt="img3" class="img-polaroid">
+			<header class="postHeader">
+				<h3>Contact Us</h3>
+			</header>
+			<p>Lerem ipsum can be a very good way to modify each string value in website</p>
+		</div>
+	</div>
 
-		<!-- navigation -->
-		<div class="span3">
+	<div class="spacer30"></div>
 
-			<?php include('sidebar-about-us.php') ?>
+	<div class="row-fluid">
+		<div class="span4">
+			<img src="http://lorempixel.com/300/150/people/3" alt="img1" class="img-polaroid">
+			<header class="postHeader">
+				<h3>Our History</h3>
+			</header>
+			<p>Lerem ipsum can be a very good way to modify each string value in website</p>
+		</div>
+		<div class="span4">
+			<img src="http://lorempixel.com/300/150/people/5" alt="img2" class="img-polaroid">
+			<header class="postHeader">
+				<h3>Founder &amp; President</h3>
+			</header>
+			<p>Lerem ipsum can be a very good way to modify each string value in website</p>
+		</div>
+		<div class="span4">
+			<img src="http://lorempixel.com/300/150/people/4" alt="img3" class="img-polaroid">
+			<header class="postHeader">
+				<h3>Partners</h3>
+			</header>
+			<p>Lerem ipsum can be a very good way to modify each string value in website</p>
+		</div>
+	</div>
 
-		</div><!-- end span -->
+	<div class="spacer30"></div>
 
-		<div class="span9">
-
-			<h1>This is about us landing page. Needs to be worked.</h1>
-
-		</div><!-- end span -->
-
+	<div class="row-fluid">
+		<div class="span4">
+			<img src="http://lorempixel.com/300/150/people/1" alt="img1" class="img-polaroid">
+			<header class="postHeader">
+				<h3>Media</h3>
+			</header>
+			<p>Lerem ipsum can be a very good way to modify each string value in website</p>
+		</div>
+		<div class="span4">
+			<img src="http://lorempixel.com/300/150/people/2" alt="img2" class="img-polaroid">
+			<header class="postHeader">
+				<h3>Online Brochure</h3>
+			</header>
+			<p>Lerem ipsum can be a very good way to modify each string value in website</p>
+		</div>
 	</div><!-- end row-fluid -->
 
 </div><!-- end container-fluid -->
