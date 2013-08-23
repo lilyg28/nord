ca8e44f3eab1e2453c4adefc5384d678404bf662
kidon-fedora-local <kidon@sterilngtrader.com>
2013-08-22 23:21:20 -0500

    Set links to each page on images and headers

--- wp-content/themes/wp-bootstrap/page-about-us.php
+++ wp-content/themes/wp-bootstrap/page-about-us.php
@@ -8,51 +8,40 @@
 	<p> The National Organization for Renal Disease is a nonprofit health agency dedicated to preventing kidney (renal) disease, diabetes and high blood pressure through education and research. </p>
 
 	<div class="row-fluid">
-		<div class="span4">
-			<img src="http://lorempixel.com/300/150/people/10" alt="img1" class="img-polaroid">
-			<header class="postHeader">
-				<h3>Our Mission</h3>
-			</header>
+		<div class="span3">
+			<a href="<?php echo home_url('about-us/mission') ?>">
+				<img src="http://lorempixel.com/200/150/people/10" alt="img1" class="img-polaroid">
+				<header class="postHeader">
+					<h3>Our Mission</h3>
+				</header>
+			</a>
 			<p>Lerem ipsum can be a very good way to modify each string value in website</p>
 		</div>
-		<div class="span4">
-			<img src="http://lorempixel.com/300/150/people/7" alt="img2" class="img-polaroid">
-			<header class="postHeader">
-				<h3>Our Program</h3>
-			</header>
+		<div class="span3">
+			<a href="<?php echo home_url('about-us/program') ?>">
+				<img src="http://lorempixel.com/200/150/people/7" alt="img2" class="img-polaroid">
+				<header class="postHeader">
+					<h3>Our Program</h3>
+				</header>
+			</a>
 			<p>Lerem ipsum can be a very good way to modify each string value in website</p>
 		</div>
-		<div class="span4">
-			<img src="http://lorempixel.com/300/150/people/8" alt="img3" class="img-polaroid">
-			<header class="postHeader">
-				<h3>Contact Us</h3>
-			</header>
+		<div class="span3">
+			<a href="<?php echo home_url('about-us/contact-us') ?>">
+				<img src="http://lorempixel.com/200/150/people/8" alt="img3" class="img-polaroid">
+				<header class="postHeader">
+					<h3>Contact Us</h3>
+				</header>
+			</a>
 			<p>Lerem ipsum can be a very good way to modify each string value in website</p>
 		</div>
-	</div>
-
-	<div class="spacer30"></div>
-
-	<div class="row-fluid">
-		<div class="span4">
-			<img src="http://lorempixel.com/300/150/people/3" alt="img1" class="img-polaroid">
-			<header class="postHeader">
-				<h3>Our History</h3>
-			</header>
-			<p>Lerem ipsum can be a very good way to modify each string value in website</p>
-		</div>
-		<div class="span4">
-			<img src="http://lorempixel.com/300/150/people/5" alt="img2" class="img-polaroid">
-			<header class="postHeader">
-				<h3>Founder &amp; President</h3>
-			</header>
-			<p>Lerem ipsum can be a very good way to modify each string value in website</p>
-		</div>
-		<div class="span4">
-			<img src="http://lorempixel.com/300/150/people/4" alt="img3" class="img-polaroid">
-			<header class="postHeader">
-				<h3>Partners</h3>
-			</header>
+		<div class="span3">
+			<a href="<?php echo home_url('about-us/partners') ?>">
+				<img src="http://lorempixel.com/200/150/people/4" alt="img3" class="img-polaroid">
+				<header class="postHeader">
+					<h3>Partners</h3>
+				</header>
+			</a>
 			<p>Lerem ipsum can be a very good way to modify each string value in website</p>
 		</div>
 	</div>
@@ -60,20 +49,47 @@
 	<div class="spacer30"></div>
 
 	<div class="row-fluid">
-		<div class="span4">
-			<img src="http://lorempixel.com/300/150/people/1" alt="img1" class="img-polaroid">
-			<header class="postHeader">
-				<h3>Media</h3>
-			</header>
+		<div class="span3">
+			<a href="<?php echo home_url('about-us/history') ?>">
+				<img src="http://lorempixel.com/200/150/people/3" alt="img1" class="img-polaroid">
+				<header class="postHeader">
+					<h3>Our History</h3>
+				</header>
+			</a>
 			<p>Lerem ipsum can be a very good way to modify each string value in website</p>
 		</div>
-		<div class="span4">
-			<img src="http://lorempixel.com/300/150/people/2" alt="img2" class="img-polaroid">
-			<header class="postHeader">
-				<h3>Online Brochure</h3>
-			</header>
+		<div class="span3">
+			<a href="<?php echo home_url('about-us/founder') ?>">
+				<img src="http://lorempixel.com/200/150/people/5" alt="img2" class="img-polaroid">
+				<header class="postHeader">
+					<h3>Founder &amp; President</h3>
+				</header>
+			</a>
 			<p>Lerem ipsum can be a very good way to modify each string value in website</p>
 		</div>
+		<div class="span3">
+			<a href="<?php echo home_url('about-us/media') ?>">
+				<img src="http://lorempixel.com/200/150/people/1" alt="img1" class="img-polaroid">
+				<header class="postHeader">
+					<h3>Media</h3>
+				</header>
+			</a>
+			<p>Lerem ipsum can be a very good way to modify each string value in website</p>
+		</div>
+		<div class="span3">
+			<a href="<?php echo home_url('about-us/brochure') ?>">
+				<img src="http://lorempixel.com/200/150/people/2" alt="img2" class="img-polaroid">
+				<header class="postHeader">
+					<h3>Online Brochure</h3>
+				</header>
+			</a>
+			<p>Lerem ipsum can be a very good way to modify each string value in website</p>
+		</div>
+	</div>
+
+	<div class="spacer30"></div>
+
+	<div class="row-fluid">
 	</div><!-- end row-fluid -->
 
 </div><!-- end container-fluid -->
